eeb286e6fff452787f6268b56f5589bda3890f0c
InnocentDusingizimana <114147827+InnocentDusingizimana@users.noreply.github.com>
2023-08-28 19:27:16 +0200

    Add files via upload

---
+++ LearnREACT.js
@@ -0,0 +1,68 @@
+
+/////////// LEARN PROPS ////////////////////////////////////
+/////////// LEARN PROPS ////////////////////////////////////
+
+
+// import './App.css';
+
+// const Person = (props) => {
+//   return (
+//     <>
+//     <h1>Name: {props.name}</h1>
+//     <h2>Last Name: {props.LastName}</h2>
+//     <h2>Age: 30</h2>
+//     </>
+//   )
+// }
+// const App = () => {
+//   return(
+//   <div className="App">
+//   <Person name = {'John'}  LastName = {'Doe'}/>
+//   <Person name = {'Innocent'} LastName = {'Dev'}/>
+//   <Person name={'Jacson'} LastName = {'Drille'}/>
+//   <Person name = {'Elon'} LastName = {'Bill'}/>
+//   <Person name = {'Mark'} LastName = {'Gate'}/>
+//   <Person name ={'Musk'} LastName = {'Zucker'}/>
+//   </div>
+
+//   );
+// }
+  
+  
+
+// export default App;
+
+/////////// LEARN PROPS ENDS HERE!!!!    ////////////////////////////////////
+/////////// LEARN PROPS ENDS HERE!!!!    ////////////////////////////////////
+
+
+
+// LEARN USESTATE AND USE EFFECT       (HOOKS) ///////////////////////////////////
+// LEARN USESTATE AND USE EFFECT       (HOOKS) ///////////////////////////////////
+
+// import { useState,useEffect } from 'react';
+// import './App.css';
+
+
+// const App = () => {
+//   const [counter, setCounter] = useState(0);
+//   useEffect(() =>{
+//     setCounter(10);
+//   }, []);
+//   return(
+//   <div className="App">
+// <button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
+// <h1>{counter}</ h1>
+// <button onClick={() => setCounter((prevCount) => prevCount +1)}>+</button>
+
+//   </div>
+
+//   );
+// }
+  
+  
+
+// export default App;
+
+// LEARN USESTATE AND USE EFFECT       (HOOKS)   ENDS HERE!!!!!!!!!    ///////////////////////////////////
+// LEARN USESTATE AND USE EFFECT       (HOOKS) ENDS HERE!!!!!!!!!    ///////////////////////////////////
